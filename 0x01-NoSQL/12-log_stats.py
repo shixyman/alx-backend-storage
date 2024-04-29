4bea1f10da919aee45991ff067bae2c9544d724c
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-29 18:13:16 +0100

    modify & add files 102

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -17,17 +17,6 @@ if __name__ == "__main__":
     delete_num = nginx_logs.count_documents({'method': 'DELETE'})
     get_status = nginx_logs.count_documents({'method': 'GET',
                                              'path': '/status'})
-    IPs_count = nginx_logs.aggregate([
-        {
-            '$group': {
-                '_id': "$ip",
-                'count': {'$sum': 1}
-            }
-        },
-        {
-            "$sort": {"count": -1}
-        }
-    ])
     print("{} logs".format(docs_num))
     print("Methods:")
     print("\tmethod GET: {}".format(get_num))
@@ -36,10 +25,3 @@ if __name__ == "__main__":
     print("\tmethod PATCH: {}".format(patch_num))
     print("\tmethod DELETE: {}".format(delete_num))
     print("{} status check".format(get_status))
-    print("IPs:")
-    x = 0
-    for i in IPs_count:
-        print("\t{}: {}".format(i.get('_id'), i.get('count')))
-        x += 1
-        if x > 9:
-            break
