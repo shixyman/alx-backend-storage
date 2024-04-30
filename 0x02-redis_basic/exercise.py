be64dfd3abe86c4d5ee0da8fb5664a72c50bfc7c
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-30 16:48:01 +0100

    test and modify main

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,50 +1,93 @@
+#!/usr/bin/env python3
+"""Module declares a redis class and methods"""
 import redis
-import uuid
+from uuid import uuid4
+from typing import Union, Callable, Optional
 from functools import wraps
-from typing import Callable, Union
 
-def call_history(method: Callable) -> Callable:
+
+def count_calls(method: Callable) -> Callable:
+    '''count how many times methods of Cache class are called'''
+    key = method.__qualname__
+
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        inputs_key = method.__qualname__ + ":inputs"
-        outputs_key = method.__qualname__ + ":outputs"
-        self._redis.rpush(inputs_key, str(args))
-        output = method(self, *args, **kwargs)
-        self._redis.rpush(outputs_key, output)
+        '''wrap the decorated function and return the wrapper'''
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    '''store the history of inputs and outputs for a particular function'''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        '''wrap the decorated function and return the wrapper'''
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
         return output
     return wrapper
 
-def replay(func: Callable):
-    inputs_key = func.__qualname__ + ":inputs"
-    outputs_key = func.__qualname__ + ":outputs"
-    inputs = [eval(arg) for arg in self._redis.lrange(inputs_key, 0, -1)]
-    outputs = self._redis.lrange(outputs_key, 0, -1)
-    print(f"{func.__qualname__} was called {len(inputs)} times:")
-    for inp, out in zip(inputs, outputs):
-        print(f"{func.__qualname__}(*{inp}) -> {out}")
+
+def replay(fn: Callable):
+    '''display the history of calls of a particular function.'''
+    r = redis.Redis()
+    func_name = fn.__qualname__
+    c = r.get(func_name)
+    try:
+        c = int(c.decode("utf-8"))
+    except Exception:
+        c = 0
+    print("{} was called {} times:".format(func_name, c))
+    inputs = r.lrange("{}:inputs".format(func_name), 0, -1)
+    outputs = r.lrange("{}:outputs".format(func_name), 0, -1)
+    for inp, outp in zip(inputs, outputs):
+        try:
+            inp = inp.decode("utf-8")
+        except Exception:
+            inp = ""
+        try:
+            outp = outp.decode("utf-8")
+        except Exception:
+            outp = ""
+        print("{}(*{}) -> {}".format(func_name, inp, outp))
+
 
 class Cache:
+    '''declares a Cache redis class'''
     def __init__(self):
-        self._redis = redis.Redis()
+        '''upon init to store an instance and flush'''
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
     @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+        '''takes a data argument and returns a string'''
+        rkey = str(uuid4())
+        self._redis.set(rkey, data)
+        return rkey
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
-        data = self._redis.get(key)
-        if data is None:
-            return None
-        if fn is not None:
-            return fn(data)
-        return data
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        '''convert the data back to the desired format'''
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
 
-    def get_str(self, key: str) -> Union[str, None]:
-        return self.get(key, fn=lambda d: d.decode("utf-8"))
+    def get_str(self, key: str) -> str:
+        '''parametrize Cache.get with correct conversion function'''
+        value = self._redis.get(key)
+        return value.decode("utf-8")
 
-    def get_int(self, key: str) -> Union[int, None]:
-        return self.get(key, fn=int)
-
+    def get_int(self, key: str) -> int:
+        '''parametrize Cache.get with correct conversion function'''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -2,53 +2,54 @@
 """ Main file"""
 
 
-# import redis
-
-# Cache = __import__('exercise').Cache
-
-# cache = Cache()
-
-# data = b"hello"
-# key = cache.store(data)
-# print(key)
-
-# local_redis = redis.Redis()
-# print(local_redis.get(key))
-
-
-
-
-# Cache = __import__('exercise').Cache
-
-# cache = Cache()
-
-# cache.store(b"first")
-# print(cache.get(cache.store.__qualname__))
-
-# cache.store(b"second")
-# cache.store(b"third")
-# print(cache.get(cache.store.__qualname__))
-
-
-# Cache = __import__('exercise').Cache
-
-# cache = Cache()
-
-# s1 = cache.store("first")
-# print(s1)
-# s2 = cache.store("secont")
-# print(s2)
-# s3 = cache.store("third")
-# print(s3)
-
-# inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
-# outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
-
-# print("inputs: {}".format(inputs))
-# print("outputs: {}".format(outputs))
+import redis
+from exercise import replay
 
 Cache = __import__('exercise').Cache
+
+
 cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
+
+
+
+
+# Cache = __import__('exercise').Cache
+
+# cache = Cache()
+
+cache.store(b"first")
+print(cache.get(cache.store.__qualname__))
+
+cache.store(b"second")
+cache.store(b"third")
+print(cache.get(cache.store.__qualname__))
+
+
+# Cache = __import__('exercise').Cache
+
+# cache = Cache()
+
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
+
+
 print(cache.store("foo"))
 print(cache.store("bar"))
 print(cache.store(42))
