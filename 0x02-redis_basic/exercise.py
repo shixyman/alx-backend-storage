e9394325b848fb8467a43ecfc378d9ebba76a9ac
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-04-30 16:22:16 +0100

    test code2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,13 +1,22 @@
-import uuid
 import redis
-from typing import Union
+import uuid
+from functools import wraps
+from typing import Callable, Union
 
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 class Cache:
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
@@ -25,4 +34,4 @@ class Cache:
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> Union[int, None]:
-        return self.get(key, fn=int)  
+        return self.get(key, fn=int)
