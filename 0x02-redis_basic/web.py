b6cf3d88001aa263f19ff2f1a8e38a92a8ea72bb
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-01 17:07:11 +0100

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,7 +2,7 @@
 import requests
 import time
 from functools import wraps
-from typing import Dict
+from typing import Dict, Callable
 
 cache: Dict[str, str] = {}
 
@@ -19,8 +19,8 @@ def get_page(url: str) -> str:
         return result
 
 
-def cache_with_expiration(expiration: int):
-    def decorator(func):
+def cache_with_expiration(expiration: int) -> Callable:
+    def decorator(func: Callable) -> Callable:
         @wraps(func)
         def wrapper(*args, **kwargs):
             url = args[0]
@@ -29,8 +29,29 @@ def cache_with_expiration(expiration: int):
                 count, timestamp = cache[key]
                 if time.time() - timestamp > expiration:
                     result = func(*args, **kwargs)
-                    cache[key] = (count+1, time.time())
+                    cache[key] = (count + 1, time.time())
                     return result
                 else:
-                    cache[key] = (count+1, timestamp)
-                    return
+                    cache[key] = (count + 1, timestamp)
+                    return None
+            else:
+                result = func(*args, **kwargs)
+                cache[key] = (1, time.time())
+                return result
+
+        return wrapper
+
+    return decorator
+
+
+@cache_with_expiration(expiration=10)
+def get_page(url: str) -> str:
+    if url in cache:
+        print(f"Retrieving from cache: {url}")
+        return cache[url]
+    else:
+        print(f"Retrieving from web: {url}")
+        response = requests.get(url)
+        result = response.text
+        cache[url] = result
+        return result
