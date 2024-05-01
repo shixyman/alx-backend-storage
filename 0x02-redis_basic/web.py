7b7348326044ab4a0e2c91c81936c143497e7279
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-01 17:14:09 +0100

    modify

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,39 +1,39 @@
-#!/usr/bin/env python3
-"""
-web cache and tracker
-"""
+#!/usr/bin/python3
+"""doc doc module"""
+
 import requests
 import redis
 from functools import wraps
 
-store = redis.Redis()
+
+# For example, the Redis server is running on localhost:6379
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
+def count_url_accesses(func):
+    """doc doc Decorator that counts how many times a URL has been accessed."""
+    @wraps(func)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        # Increment the count each time a URL is accessed
+        count_key = f"count:{url}"
+        redis_client.incr(count_key)
+        return func(url)
     return wrapper
 
 
-@count_url_access
+@count_url_accesses
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """doc doc Fetches HTML content of URL with caching and access counting"""
+    cache_key = f"cache:{url}"
+    # Check if the cached version exists
+    cached_content = redis_client.get(cache_key)
+    if cached_content:
+        return cached_content.decode('utf-8')
+
+    # If not cached, fetch the content and cache it with a 10-second expiration
+    response = requests.get(url)
+    redis_client.setex(cache_key, 10, response.text)
+    return response.text
 
 
 if __name__ == "__main__":
