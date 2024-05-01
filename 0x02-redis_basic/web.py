7fb5e5308dc64a25ab05deb91fbdab4806ce1d38
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-01 17:09:24 +0100

    modify

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,62 +1,40 @@
 #!/usr/bin/env python3
+"""
+web cache and tracker
+"""
 import requests
-import time
+import redis
 from functools import wraps
-from typing import Dict, Callable
 
-cache: Dict[str, str] = {}
+store = redis.Redis()
 
 
+def count_url_access(method):
+    """ Decorator counting how many times
+    a URL is accessed """
+    @wraps(method)
+    def wrapper(url):
+        cached_key = "cached:" + url
+        cached_data = store.get(cached_key)
+        if cached_data:
+            return cached_data.decode("utf-8")
+
+        count_key = "count:" + url
+        html = method(url)
+
+        store.incr(count_key)
+        store.set(cached_key, html)
+        store.expire(cached_key, 10)
+        return html
+    return wrapper
+
+
+@count_url_access
 def get_page(url: str) -> str:
-    if url in cache:
-        print(f"Retrieving from cache: {url}")
-        return cache[url]
-    else:
-        print(f"Retrieving from web: {url}")
-        response = requests.get(url)
-        result = response.text
-        cache[url] = result
-        return result
-
-
-def cache_with_expiration(expiration: int) -> Callable:
-    def decorator(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            url = args[0]
-            key = f"count:{url}"
-            if key in cache:
-                count, timestamp = cache[key]
-                if time.time() - timestamp > expiration:
-                    result = func(*args, **kwargs)
-                    cache[key] = (count + 1, time.time())
-                    return result
-                else:
-                    cache[key] = (count + 1, timestamp)
-                    return None
-            else:
-                result = func(*args, **kwargs)
-                cache[key] = (1, time.time())
-                return result
-
-        return wrapper
-
-    return decorator
-
-
-@cache_with_expiration(expiration=10)
-def get_page(url: str) -> str:
-    if url in cache:
-        print(f"Retrieving from cache: {url}")
-        return cache[url]
-    else:
-        print(f"Retrieving from web: {url}")
-        response = requests.get(url)
-        result = response.text
-        cache[url] = result
-        return result
+    """ Returns HTML content of a url """
+    res = requests.get(url)
+    return res.text
 
 
 if __name__ == "__main__":
-
     get_page('http://slowwly.robertomurray.co.uk')
