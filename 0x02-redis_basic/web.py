f5452a1ecb09600335bd627b4765f3d31544831d
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-01 16:55:29 +0100

    testing

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -24,7 +24,8 @@ def data_cacher(method: Callable) -> Callable:
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_store.set(f'result:{url}', result, ex=10)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
 
