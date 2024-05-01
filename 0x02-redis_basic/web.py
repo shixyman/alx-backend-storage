9129ff2417adc4c05797b05b66001baa1aafe379
Y'Usri R'Chid <shixymanflowx@gmail.com>
2024-05-01 17:18:10 +0100

    modify

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python3
 """doc doc module"""
-
 import requests
 import redis
 from functools import wraps
 
-
-# For example, the Redis server is running on localhost:6379
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
 
 def count_url_accesses(func):
-    """doc doc Decorator that counts how many times a URL has been accessed."""
     @wraps(func)
     def wrapper(url):
-        # Increment the count each time a URL is accessed
         count_key = f"count:{url}"
         redis_client.incr(count_key)
         return func(url)
+
     return wrapper
 
 
-@count_url_accesses
-def get_page(url: str) -> str:
-    """doc doc Fetches HTML content of URL with caching and access counting"""
-    cache_key = f"cache:{url}"
-    # Check if the cached version exists
-    cached_content = redis_client.get(cache_key)
-    if cached_content:
-        return cached_content.decode('utf-8')
+def cache_with_expiration(expiration):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(url):
+            cache_key = f"cache:{url}"
+            cached_content = redis_client.get(cache_key)
+            if cached_content:
+                return cached_content.decode('utf-8')
+            else:
+                response = func(url)
+                redis_client.setex(cache_key, expiration, response)
+                return response
 
-    # If not cached, fetch the content and cache it with a 10-second expiration
+        return wrapper
+
+    return decorator
+
+
+@count_url_accesses
+@cache_with_expiration(expiration=10)
+def get_page(url: str) -> str:
     response = requests.get(url)
-    redis_client.setex(cache_key, 10, response.text)
     return response.text
 
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    url = "http://slowwly.robertomurray.co.uk"
+    content = get_page(url)
+    print(content)
